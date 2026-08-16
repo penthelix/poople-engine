d4bdf3b5e76987e67f41e59a5ecd4309c73a2969
helix <commits.cider786@passfwd.com>
2026-08-16 11:18:53 +0530

    feat: implement filter_words_by_length function in data.py and PROJECT_ROOT constant in utils.py

---
+++ src/data.py
@@ -0,0 +1,39 @@
+from pathlib import Path
+
+from src.utils import PROJECT_ROOT
+
+WORD_LENGTH: int = 4
+
+
+def filter_words_by_length(
+    in_path: Path,
+    word_length: int = WORD_LENGTH,
+    out_dir: Path | None = None,
+) -> int:
+    # Validate paths
+    if not in_path.exists() or not in_path.is_file():
+        raise FileNotFoundError(f"{in_path} was not found.")
+    if out_dir and not out_dir.is_dir():
+        raise NotADirectoryError(f"{out_dir} must be a directory.")
+
+    out_filename = f"{word_length}_letter_words.txt"
+    out_path = Path()
+
+    if out_dir:
+        out_path = out_dir / out_filename
+    else:
+        out_path = Path(".").parent.parent.absolute() / "data" / out_filename
+    out_path.touch()
+
+    out_line_count: int = 0
+
+    with open(in_path, "r") as fin, open(out_path, "w") as fout:
+        for line in fin:
+            if word_length == len(line.strip()):
+                out_line_count += 1
+                _ = fout.write(line)
+    return out_line_count
+
+
+if __name__ == "__main__":
+    _ = filter_words_by_length(PROJECT_ROOT / "data" / "all_words.txt")
