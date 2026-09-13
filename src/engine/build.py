c753c14874e0ee5677284aa5ce7f11597cff79d2
helix <commits.cider786@passfwd.com>
2026-09-14 01:43:05 +0530

    fix: change generated executable name cli -> poople

--- src/engine/build.py
+++ src/engine/build.py
@@ -5,4 +5,4 @@ from PyInstaller.__main__ import run
 
 def main() -> None:
     data_arg: str = f"data/all_words.txt{os.pathsep}data"
-    run(["src/engine/cli.py", "--add-data", data_arg])
+    run(["src/engine/cli.py", "--add-data", data_arg, "--name", "poople"])
